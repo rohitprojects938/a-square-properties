c1b78eb1eb6fc5e9848190239ca9a37cf0c20731
rohitproject938 <rohitcreation12345@gmail.com>
2026-07-28 13:20:22 +0530

    Bust browser cache and update asset versioning to v134 for category height alignment

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'houserenter-pwa-cache-v133';
+const CACHE_NAME = 'houserenter-pwa-cache-v134';
 const ASSETS = [
   '/',
   '/index.html',
   '/login.html',
-  '/css/style.v133.css',
-  '/js/main.v133.js',
+  '/css/style.v134.css',
+  '/js/main.v134.js',
   '/uploads/profile/default-avatar.png'
 ];
 
