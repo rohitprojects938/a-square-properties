54a174abbf4a10f6f1866fecbe0df0adf4df872b
rohitproject938 <rohitcreation12345@gmail.com>
2026-07-28 15:38:53 +0530

    fix: increase modal overlay z-index to 10000 to overlay bottom navigation bar and fix click interception, version bump to v145

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'houserenter-pwa-cache-v144';
+const CACHE_NAME = 'houserenter-pwa-cache-v145';
 const ASSETS = [
   '/',
   '/index.html',
   '/login.html',
-  '/css/style.v144.css',
-  '/js/main.v144.js',
+  '/css/style.v145.css',
+  '/js/main.v145.js',
   '/uploads/profile/default-avatar.png'
 ];
 
